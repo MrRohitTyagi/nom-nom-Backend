8daf24806fb848cdc8721281f1fee94d725746d7
Rohit Tyagi <rohit.tyagi@thoughts2binary.com>
2024-03-17 04:23:55 +0530

    cors issue

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const app = express();
 connectToDB(MONGO_STRING);
 app.use(express.json());
 app.use(cors({ origin: true, methods: "GET,HEAD,PUT,PATCH,POST,DELETE" }));
-app.options("*", cors());
+app.options("*", cors("*"));
+app.use(cors("*"));
 
 app.use("/api/v1/user", userRouter);
 
