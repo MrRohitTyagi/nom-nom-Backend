24ff4364251d43aeb8a839eaf10cf92186f7c938
Rohit Tyagi <rohit.tyagi@thoughts2binary.com>
2024-03-18 10:22:01 +0530

    auth handlers added

--- handlers/userHandler.js
+++ handlers/userHandler.js
@@ -1,50 +1,5 @@
 import userModel from "../models/userModel.js";
-import { generateToken } from "../utils/jsonwentoken.js";
-import bcrypt from "bcryptjs";
 
-export const signUp = async (req, res) => {
-  try {
-    let user = await userModel.create(req.body);
-    user = user.toObject();
-
-    delete user.password;
-    delete user.__v;
-
-    const token = generateToken(user);
-    res.send({ success: true, user, token });
-  } catch (error) {
-    res
-      .status(500)
-      .send({ success: false, msg: error?.message || "Something went wrong" });
-  }
-};
-
-export const login = async (req, res) => {
-  const { email = "", password = "" } = req.body;
-  try {
-    let user = await userModel.findOne({ email: email.trim() });
-    if (!user) return res.status(400).send({ msg: "User does not exists" });
-    user = user.toObject();
-    //if user is found by email
-    const isPasswordSame = await bcrypt.compare(password, user.password);
-    if (!isPasswordSame) {
-      return res.status(400).send({
-        msg: "Password does not match ,please enter the correct password",
-      });
-    }
-
-    delete user.password;
-    delete user.__v;
-
-    const token = generateToken(user);
-
-    res.send({ success: true, user, token });
-  } catch (error) {
-    res
-      .status(500)
-      .send({ success: false, msg: error?.message || "Something went wrong" });
-  }
-};
 
 export const getUserByToken = async (req, res) => {
   try {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,14 @@
 import express from "express";
-import {
-  getUserByToken,
-  login,
-  signUp,
-  updateUser,
-} from "../handlers/userHandler.js";
+import { getUserByToken, updateUser } from "../handlers/userHandler.js";
 import { VerifyToken } from "../utils/jsonwentoken.js";
+import { login, signUp } from "../handlers/authHandlers.js";
 const userRouter = express.Router();
 
 //post User
 userRouter.post("/signup", signUp);
 userRouter.post("/login", login);
+userRouter.post("/regester-restraunt", login);
+
 userRouter.get("/get", tokenVerifyMiddleware, getUserByToken);
 userRouter.put("/update", tokenVerifyMiddleware, updateUser);
 
